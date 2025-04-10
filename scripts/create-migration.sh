ee6a859f188e8a80175481d52a2e5dd9c80cb65b
Munir Muratovic <munirmuratovicx@gmail.com>
2025-04-10 22:16:40 +0200

    Move migration scripts to /scripts folder

--- scripts/create-migration.sh
+++
@@ -1,3 +0,0 @@
-dotnet ef migrations add InitialMigration --project UltimateClean.Persistence/UltimateClean.Persistence.csproj --startup-project UltimateClean.Api/UltimateClean.Api.csproj --output-dir ./Migrations
-
-dotnet ef database update --verbose --project UltimateClean.Persistence/UltimateClean.Persistence.csproj --startup-project UltimateClean.Api/UltimateClean.Api.csproj
